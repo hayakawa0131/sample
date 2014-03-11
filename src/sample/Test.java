ed7dce6502dd1c4a4a771927203edfa58ed021ea
hayakawa <hayakawa0131@gmail.com>
2014-03-11 17:21:11 +0900

    Test1

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -1,9 +1,16 @@
 package sample;
 
 public class Test {
+	
+	public static int a = 3;
 
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Worlod");
+		
+		a += 5;
+		System.out.println(a);
+		
+		
 	}
 }

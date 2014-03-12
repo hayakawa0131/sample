2420a5966cfc0de683f81a2eb7c0fda1d5cb85ff
Hayakawa Ryo <hayakawa0131@gmail.com>
2014-03-12 14:35:04 +0900

    Gitの基本をマスターした.

--- src/sample/MathPractice.java
+++ src/sample/MathPractice.java
@@ -1,5 +1,13 @@
 package sample;
 
 public class MathPractice {
+	
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+		int c;
+		c = a + b;
+		System.out.println(c);
+	}
 
 }
